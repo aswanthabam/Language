c1c4c149cfdde81b8006896bd8bfcffa26a0720e
aswanthabam <aswanthabam@gmail.com>
2020-09-13 21:59:24 +0530

    modified contents

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Language", # Replace with your own username
+    name="HyperTextMarkup", # Replace with your own username
     version="1.0.0",
     author="Aswanth Vc",
-    author_email="aswanthvcabam@gmail.com",
-    description="Programming language parser. You can parse HyperTextMarkupLanguage (html) using this package.",
+    author_email="no-mail@mail.com",
+    description="Programming language decoder. Now You can Decode HyperTextMarkupLanguage (html) using this package.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
